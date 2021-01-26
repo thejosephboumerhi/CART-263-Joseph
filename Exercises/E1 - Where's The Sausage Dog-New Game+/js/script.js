7b637fb2c7d8809398790e699aab928faaca5f91
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-01-26 17:35:56 -0500

    E1: Starting code

---
+++ Exercises/E1 - Where's The Sausage Dog-New Game+/js/script.js
@@ -0,0 +1,55 @@
+/**************************************************
+A1 - Where's The Sausage Dog
+Joseph Boumerhi
+
+Here is a description of this template p5 project.
+**************************************************/
+`use strict`;
+
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMAL = 100;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < NUM_ANIMAL; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
+}
+
+function draw() {
+  background(255, 255, 0);
+
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed() {
+  sausageDog.mousePressed();
+}
