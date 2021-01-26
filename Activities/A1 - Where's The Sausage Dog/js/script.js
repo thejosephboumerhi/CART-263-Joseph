46ed57db98319ce9483e3b20bb2345334d15503d
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-01-26 16:20:01 -0500

    A1: Finished A1

--- Activities/A1 - Where's The Sausage Dog/js/script.js
+++ Activities/A1 - Where's The Sausage Dog/js/script.js
@@ -1,20 +1,55 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+A1 - Where's The Sausage Dog
+Joseph Boumerhi
 
 Here is a description of this template p5 project.
 **************************************************/
+`use strict`;
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMAL = 100;
 
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`);
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  for (let i = 0; i < NUM_ANIMAL; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
+}
+
+function draw() {
+  background(255, 255, 0);
+
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed() {
+  sausageDog.mousePressed();
 }
