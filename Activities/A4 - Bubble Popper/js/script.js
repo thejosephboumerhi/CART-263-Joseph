78a3170bed6ddb4222fc02ada787f24c679fa406
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-02-15 19:46:39 -0500

    A4: Saving work

--- Activities/A4 - Bubble Popper/js/script.js
+++ Activities/A4 - Bubble Popper/js/script.js
@@ -1,32 +1,32 @@
 "use strict";
 
 /**
-Title of Project
-Author Name
+A4 - Bubble Popper
+Joseph Boumerhi
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
-/**
-Description of preload
-*/
-function preload() {
+let video = undefined;
 
-}
+let handpose = undefined;
 
+let predictions = [];
 
-/**
-Description of setup
-*/
 function setup() {
+  createCanvas(640,480);
+
+//Access user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+//Loads handpose model
+  handpose = ml5.handpose(video, {flipHorizontal: true}, function() {console.log('Model loaded');};});
 
 }
-
 
 /**
 Description of draw()
 */
-function draw() {
-
-}
+function draw() {}
