570cf136d52c9d5a223269c775b35a72ef56723a
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-02-16 21:23:42 -0500

    E4: Copy and pasting usual text boxes, and state order (title -> game -> end), while rearranging some code around.

--- Exercises/E4 - Bubble Popper++/js/script.js
+++ Exercises/E4 - Bubble Popper++/js/script.js
@@ -7,6 +7,7 @@ Joseph Boumerhi
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
+let state = `title`;
 
 let video = undefined;
 
@@ -15,6 +16,8 @@ let handpose = undefined;
 let predictions = [];
 
 let bubble = undefined;
+
+let poisonBubble = undefined;
 
 function setup() {
   createCanvas(640, 480);
@@ -40,14 +43,36 @@ function setup() {
     vx: 0,
     vy: -1,
   };
+
+  poisonBubble = {
+    x: random(width),
+    y: height,
+    size: 100,
+    vx: 0,
+    vy: -1,
+  };
 }
 
 /**
 Description of draw()
 */
 function draw() {
-  background(0);
+  if (state === `title`) {
+    background(100, 200, 100);
+    title();
+  } else if (state === `bubblePoppin`) {
+    background(0, 163, 84);
+    mLfingerPopper();
+  } else if (state === `poppinChampion`) {
+    background(255);
+    winScreen();
+  } else if (state === `bubblePoppinBaby`) {
+    background(0);
+    endScreen();
+  }
+}
 
+function mLfingerPopper() {
   if (predictions.length > 0) {
     let hand = predictions[0];
     let index = hand.annotations.indexFinger;
@@ -90,4 +115,62 @@ function draw() {
   noStroke();
   ellipse(bubble.x, bubble.y, bubble.size);
   pop();
+}
+
+function bubbleRegistration() {
+  let d = dist(tipX, tipY, bubble.x, bubble.y);
+  if (d < bubble.size / 2) {
+    bubble.x = random(width);
+    bubble.y = height;
+  }
+
+  //Title screen text
+  function title() {
+    push();
+    textSize(30);
+    fill(0, 208, 212);
+    stroke(0);
+    strokeWeight(5);
+    textAlign(CENTER, CENTER);
+    text(`Bubble Popper++`, width / 2, height / 2);
+    pop();
+  }
+
+  //Text if you cannot find the dog in time
+  function endScreen() {
+    push();
+    textSize(30);
+    fill(255, 0, 0);
+    stroke(0);
+    strokeWeight(5);
+    textAlign(CENTER, CENTER);
+    text(`You intoxicated yourself!`, width / 2, height / 2);
+    pop();
+  }
+
+  //Text for when you click the dog in time
+  function winScreen() {
+    push();
+    textSize(30);
+    fill(235, 216, 52);
+    stroke(0);
+    strokeWeight(5);
+    textAlign(CENTER, CENTER);
+    text(`You popped bubbles for long enough`, width / 2, height / 2);
+    pop();
+  }
+
+  function mousePressed() {
+    if (state === `title`) {
+      state = `questionnaire`;
+    } else if (state === `questionnaire`) {
+      currentAnimal = random(animals);
+      let reverseAnimal = reverseString(currentAnimal);
+      responsiveVoice.speak(reverseAnimal);
+    } else if (state === `wonQuiz`) {
+      state = `title`;
+    } else if (state === `lostQuiz`) {
+      state = `title`;
+    }
+  }
 }
