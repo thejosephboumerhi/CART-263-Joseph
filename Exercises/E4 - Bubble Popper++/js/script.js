e446e2980f27d81557c36619139b3d0a0e483ffe
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-02-17 01:50:36 -0500

    E4: Added more text, and text counters for points and lives.

--- Exercises/E4 - Bubble Popper++/js/script.js
+++ Exercises/E4 - Bubble Popper++/js/script.js
@@ -8,21 +8,24 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 let state = `title`;
+let instructions = `Use your index finger to pop the bubbles, click to start
+Blues give points
+Green gives lifes
+Purple hurts`;
 
 let video = undefined;
 
 let handpose = undefined;
-
 let predictions = [];
 
 let bubble = undefined;
-
 let poisonBubble = undefined;
 
 let lives = 3;
+let bubblePoints = 6;
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 600);
 
   //Access user's webcam
   video = createCapture(VIDEO);
@@ -60,14 +63,14 @@ Description of draw()
 */
 function draw() {
   if (state === `title`) {
-    background(100, 200, 100);
+    background(0, 2, 97);
     title();
   } else if (state === `bubblePoppin`) {
-    background(0, 163, 84);
+    background(3, 1, 105);
     mLfingerPopper();
     bubbleDisplay();
     bubbleMovement();
-    bubbleRegistration();
+    playerCounter();
   } else if (state === `poppinChampion`) {
     background(255);
     winScreen();
@@ -87,6 +90,7 @@ function mLfingerPopper() {
     let tipY = tip[1];
     let baseX = base[0];
     let baseY = base[1];
+
     push();
     noFill();
     stroke(255, 255, 255);
@@ -99,6 +103,21 @@ function mLfingerPopper() {
     fill(255, 0, 0);
     ellipse(baseX, baseY, 20);
     pop();
+
+    //Bubble hit registration
+    //Detection for blue bubble
+    let d = dist(tipX, tipY, bubble.x, bubble.y);
+    if (d < bubble.size / 2) {
+      bubble.x = random(width);
+      bubble.y = height;
+    }
+
+    //Detection for purple bubble
+    let toxic = dist(tipX, tipY, poisonBubble.x, poisonBubble.y);
+    if (toxic < poisonBubble.size / 2) {
+      poisonBubble.x = random(width);
+      poisonBubble.y = height;
+    }
   }
 }
 
@@ -134,33 +153,34 @@ function bubbleMovement() {
   }
 }
 
-function bubbleRegistration() {
-  let d = dist(tipX, tipY, bubble.x, bubble.y);
-  if (d < bubble.size / 2) {
-    bubble.x = random(width);
-    bubble.y = height;
-  }
-
-  let toxic = dist(tipX, tipY, poisonBubble.x, poisonBubble.y);
-  if (toxic < poisonBubble.size / 2) {
-    poisonBubble.x = random(width);
-    poisonBubble.y = height;
-  }
-}
-
 //Title screen text
 function title() {
   push();
   textSize(30);
-  fill(0, 208, 212);
+  fill(0, 198, 201);
   stroke(0);
   strokeWeight(5);
   textAlign(CENTER, CENTER);
   text(`Bubble Popper++`, width / 2, height / 2);
+  textSize(15);
+  fill(182, 207, 0);
+  text(instructions, width / 2, height / 1.5);
   pop();
 }
 
-//Text if you pop the wrong bubble
+function playerCounter() {
+  push();
+  textSize(30);
+  fill(255);
+  stroke(0);
+  strokeWeight(5);
+  textAlign(LEFT, BOTTOM);
+  text(`Lives: ${lives}`, width / 20, 550);
+  text(`Bubbles Popped: ${bubblePoints}`, width / 20, 575);
+  pop();
+}
+
+//Text if you pop enough of the wrong bubbles
 function endScreen() {
   push();
   textSize(30);
