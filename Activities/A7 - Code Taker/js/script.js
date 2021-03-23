daab57bf3da498f29d57d9d271c44e955388d8a4
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-03-23 16:32:01 -0400

    A7: Finished A7

--- Activities/A7 - Code Taker/js/script.js
+++ Activities/A7 - Code Taker/js/script.js
@@ -1,11 +1,35 @@
 /**
-Title of Project
-Author Name
+A7 - Code Taker
+Joseph Boumerhi
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Poem
 */
 
 "use strict";
 
-// Code goes here
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
+
+$(`.secret`).one(`mouseover`, function (event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({ helper: `clone` });
+});
+
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    //Check if got
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
