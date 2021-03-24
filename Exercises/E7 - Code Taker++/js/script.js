8ce72f25ef1dd6b5c64aab35cd914e9eaf8ac239
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-03-23 22:12:49 -0400

    E7: Added modal on startup, thanks to Dana

---
+++ Exercises/E7 - Code Taker++/js/script.js
@@ -0,0 +1,39 @@
+/**
+A7 - Code Taker
+Joseph Boumerhi
+
+Poem
+*/
+
+"use strict";
+
+$(`#instruction-dialog`).dialog({
+  modal: true,
+});
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
+
+$(`.secret`).one(`mouseover`, function (event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({ helper: `clone` });
+});
+
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    //Check if got
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
