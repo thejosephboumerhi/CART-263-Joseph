f9803d454bad398d704de5e4c18a4216723c30ba
Joseph Boumerhi <joeyboumerhi@gmail.com>
2021-04-23 23:58:05 -0400

    E7: Finished E7, last touches

--- Exercises/E7 - Code Taker++/js/script.js
+++ Exercises/E7 - Code Taker++/js/script.js
@@ -9,18 +9,20 @@ Poem
 
 //Dana showed me (should make more use of looking at things in general),
 //https://pippinbarr.github.io/cart263-2021/topics/jquery-uI/jquery-ui-widgets.html
-//Brings up dialog box on startup
+//Brings up dialog box on startup, essentially just the instructions
 $(`#instruction-dialog`).dialog({
   modal: true,
 });
 
+//Lil shake on startup to hint
 $(`.secret`).effect({
   effect: `shake`,
   duration: 500,
-  times: 10,
+  times: 5,
   distance: 2,
 });
 
+//After finding "shit", they'll be a concluding prompt
 $(`#solved-dialog`).dialog({
   autoOpen: false,
   buttons: {
@@ -30,11 +32,13 @@ $(`#solved-dialog`).dialog({
   },
 });
 
+//The "secret" letters will light up light blue and underlined for you to drag
 $(`.secret`).one(`mouseover`, function (event) {
   $(this).addClass(`found`, 500);
   $(this).draggable({ helper: `clone` });
 });
 
+//And drop the draggable letters, to form "shit", and a prompt will be triggered
 $(`#answer`).droppable({
   drop: function (event, ui) {
     let letter = ui.draggable.text();
